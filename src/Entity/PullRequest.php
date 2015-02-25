a1bcf363938579a2f090713fff61e240bf0a6e28
Andreas Möller <am@localheinz.com>
2015-02-25 15:33:17 +0100

    Enhancement: Implement Entity\PullRequest

---
+++ src/Entity/PullRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Localheinz\ChangeLog\Entity;
+
+class PullRequest
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @param string $id
+     * @param string $title
+     */
+    public function __construct($id, $title)
+    {
+        $this->id = $id;
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function id()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function title()
+    {
+        return $this->title;
+    }
+}
---
+++ tests/Entity/PullRequestTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Localheinz\ChangeLog\Test\Entity;
+
+use Localheinz\ChangeLog\Entity;
+use PHPUnit_Framework_TestCase;
+
+class PullRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructorSetsIdAndTitle()
+    {
+        $id = '9000';
+        $title = 'Enhancement: Reduce memory usage';
+
+        $entity = new Entity\PullRequest(
+            $id,
+            $title
+        );
+
+        $this->assertSame($id, $entity->id());
+        $this->assertSame($title, $entity->title());
+    }
+}
