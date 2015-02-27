a74a38318f277daad2054656ecd3228e302fbe69
Andreas Möller <am@localheinz.com>
2015-02-27 13:52:52 +0100

    Enhancement: Move range() into Service\Commit

--- src/Service/Commit.php
+++ src/Service/Commit.php
@@ -2,6 +2,89 @@
 
 namespace Localheinz\ChangeLog\Service;
 
+use Localheinz\ChangeLog\Entity;
+use Localheinz\ChangeLog\Repository;
+
 class Commit
 {
+    /**
+     * @var Repository\Commits
+     */
+    private $repository;
+
+    public function __construct(Repository\Commits $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param string $userName
+     * @param string $repository
+     * @param string $startReference
+     * @param string $endReference
+     * @return Entity\Commit[]
+     */
+    public function range($userName, $repository, $startReference, $endReference)
+    {
+        if ($startReference === $endReference) {
+            return [];
+        }
+
+        $start = $this->repository->show(
+            $userName,
+            $repository,
+            $startReference
+        );
+
+        if (null === $start) {
+            return [];
+        }
+
+        $end = $this->repository->show(
+            $userName,
+            $repository,
+            $endReference
+        );
+
+        if (null === $end) {
+            return [];
+        }
+
+        $commits = $this->repository->all($userName, $repository, [
+            'sha' => $start->sha(),
+        ]);
+
+        if (!is_array($commits)) {
+            return [];
+        }
+
+        $range = [];
+
+        $currentStart = $start;
+
+        while (count($commits)) {
+            /* @var Entity\Commit $commit */
+            $commit = array_shift($commits);
+
+            if ($commit->sha() === $currentStart->sha()) {
+                continue;
+            }
+
+            array_push($range, $commit);
+
+            if ($commit->sha() === $end->sha()) {
+                break;
+            }
+
+            if (!count($commits)) {
+                $currentStart = $commit;
+
+                $commits = $this->repository->all($userName, $repository, [
+                    'sha' => $currentStart->sha(),
+                ]);
+            }
+        }
+
+        return $range;
+    }
 }
---
+++ tests/Service/CommitTest.php
@@ -0,0 +1,409 @@
+<?php
+
+namespace Localheinz\ChangeLog\Test\Service;
+
+use Localheinz\ChangeLog\Entity;
+use Localheinz\ChangeLog\Repository;
+use Localheinz\ChangeLog\Service;
+use Localheinz\ChangeLog\Test\Util\FakerTrait;
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+
+class CommitTest extends PHPUnit_Framework_TestCase
+{
+    use FakerTrait;
+
+    public function testRangeDoesNotFetchCommitsIfStartAndEndReferencesAreTheSame()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+
+        $endSha = $startSha;
+
+        $commitRepository = $this->commitRepository();
+
+        $commitRepository
+            ->expects($this->never())
+            ->method($this->anything())
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $commits = $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+
+        $this->assertSame([], $commits);
+    }
+
+    public function testRangeDoesNotFetchCommitsIfStartCommitCouldNotBeFound()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+        $endSha = '7fc1c4f';
+
+        $commitRepository = $this->commitRepository();
+
+        $commitRepository
+            ->expects($this->at(0))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($startSha)
+            )
+            ->willReturn(null)
+        ;
+
+        $commitRepository
+            ->expects($this->never())
+            ->method('all')
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $commits = $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+
+        $this->assertSame([], $commits);
+    }
+
+    public function testRangeDoesNotFetchCommitsIfEndCommitCouldNotBeFound()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+        $endSha = '7fc1c4f';
+
+        $commitRepository = $this->commitRepository();
+
+        $commitRepository
+            ->expects($this->at(0))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($startSha)
+            )
+            ->willReturn($this->commit())
+        ;
+
+        $commitRepository
+            ->expects($this->at(1))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($endSha)
+            )
+            ->willReturn(null)
+        ;
+
+        $commitRepository
+            ->expects($this->never())
+            ->method('all')
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $commits = $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+
+        $this->assertSame([], $commits);
+    }
+
+    public function testRangeFetchesCommitsUsingShaFromStartCommit()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+        $endSha = '7fc1c4f';
+
+        $commitRepository = $this->commitRepository();
+
+        $startCommit = $this->commit();
+
+        $commitRepository
+            ->expects($this->at(0))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($startSha)
+            )
+            ->willReturn($startCommit)
+        ;
+
+        $commitRepository
+            ->expects($this->at(1))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($endSha)
+            )
+            ->willReturn($this->commit())
+        ;
+
+        $commitRepository
+            ->expects($this->once())
+            ->method('all')
+            ->with($this->equalTo($userName), $this->equalTo($repository), $this->equalTo([
+                'sha' => $startCommit->sha(),
+            ]))
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+    }
+
+    public function testRangeReturnsArrayOfCommitsFromStartToEndExcludingStart()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+        $endSha = '7fc1c4f';
+
+        $commitRepository = $this->commitRepository();
+
+        $startCommit = $this->commit();
+
+        $commitRepository
+            ->expects($this->at(0))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($startSha)
+            )
+            ->willReturn($startCommit)
+        ;
+
+        $endCommit = $this->commit();
+
+        $commitRepository
+            ->expects($this->at(1))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($endSha)
+            )
+            ->willReturn($endCommit)
+        ;
+
+        $countBetween = 13;
+        $countAfter = 17;
+
+        $allCommits = [
+            $startCommit,
+        ];
+
+        $this->addCommits($allCommits, $countBetween);
+
+        array_push($allCommits, $endCommit);
+
+        $this->addCommits($allCommits, $countAfter);
+
+        $expectedCommits = array_slice(
+            $allCommits,
+            1,
+            $countBetween + 1
+        );
+
+        $commitRepository
+            ->expects($this->once())
+            ->method('all')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo([
+                    'sha' => $startCommit->sha(),
+                ])
+            )
+            ->willReturn($allCommits)
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $commits = $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+
+        $this->assertCount(count($expectedCommits), $commits);
+
+        foreach ($commits as $commit) {
+            $expectedCommit = array_shift($expectedCommits);
+            $this->assertSame($expectedCommit, $commit);
+        }
+    }
+
+    public function testRangeFetchesMoreCommitsIfEndIsNotContainedInFirstBatch()
+    {
+        $userName = 'foo';
+        $repository = 'bar';
+        $startSha = 'ad77125';
+        $endSha = '7fc1c4f';
+
+        $commitRepository = $this->commitRepository();
+
+        $startCommit = $this->commit();
+
+        $commitRepository
+            ->expects($this->at(0))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($startSha)
+            )
+            ->willReturn($startCommit)
+        ;
+
+        $endCommit = $this->commit();
+
+        $commitRepository
+            ->expects($this->at(1))
+            ->method('show')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo($endSha)
+            )
+            ->willReturn($endCommit)
+        ;
+
+        $firstBatch = [
+            $startCommit,
+        ];
+
+        $this->addCommits($firstBatch, 50);
+
+        $lastCommitFromFirstBatch = end($firstBatch);
+        reset($firstBatch);
+
+        $secondBatch = [
+            $lastCommitFromFirstBatch,
+        ];
+
+        $this->addCommits($secondBatch, 20);
+
+        array_push($secondBatch, $endCommit);
+
+        $expectedCommits = array_merge(
+            array_slice(
+                $firstBatch,
+                1
+            ),
+            array_slice(
+                $secondBatch,
+                1
+            )
+        );
+
+        reset($expectedCommits);
+
+        $commitRepository
+            ->expects($this->at(2))
+            ->method('all')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo([
+                    'sha' => $startCommit->sha(),
+                ])
+            )
+            ->willReturn($firstBatch)
+        ;
+
+        $commitRepository
+            ->expects($this->at(3))
+            ->method('all')
+            ->with(
+                $this->equalTo($userName),
+                $this->equalTo($repository),
+                $this->equalTo([
+                    'sha' => $lastCommitFromFirstBatch->sha(),
+                ])
+            )
+            ->willReturn($secondBatch)
+        ;
+
+        $service = new Service\Commit($commitRepository);
+
+        $commits = $service->range(
+            $userName,
+            $repository,
+            $startSha,
+            $endSha
+        );
+
+        $this->assertCount(count($expectedCommits), $commits);
+
+        foreach ($commits as $commit) {
+            $expectedCommit = array_shift($expectedCommits);
+
+            $this->assertSame($expectedCommit, $commit);
+        }
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function commitRepository()
+    {
+        return $this->getMockBuilder(Repository\Commits::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+
+    /**
+     * @param string $sha
+     * @param string $message
+     * @return Entity\Commit
+     */
+    private function commit($sha = null, $message = null)
+    {
+        $sha = $sha ?: $this->faker()->unique()->sha1;
+        $message = $message ?: $this->faker()->unique()->sentence();
+
+        return new Entity\Commit(
+            $sha,
+            $message
+        );
+    }
+
+    /**
+     * @param Entity\Commit[] $commits
+     * @param int $count
+     */
+    private function addCommits(&$commits, $count)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            array_push($commits, $this->commit());
+        }
+    }
+}
