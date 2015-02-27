37e3b0be622b8fe19b99cf0352c50ec821c7f5b7
Andreas Möller <am@localheinz.com>
2015-02-27 17:52:52 +0100

    Fix: Remove code not covered by tests

--- src/Service/PullRequest.php
+++ src/Service/PullRequest.php
@@ -48,50 +48,6 @@ class PullRequest
     }
 
     /**
-     * @param string $user
-     * @return self
-     */
-    public function userName($user)
-    {
-        $this->userName = $user;
-
-        return $this;
-    }
-
-    /**
-     * @param string $repository
-     * @return self
-     */
-    public function repository($repository)
-    {
-        $this->repository = $repository;
-
-        return $this;
-    }
-
-    /**
-     * @param string $startSha
-     * @return self
-     */
-    public function startSha($startSha)
-    {
-        $this->startSha = $startSha;
-
-        return $this;
-    }
-
-    /**
-     * @param string $endSha
-     * @return self
-     */
-    public function endSha($endSha)
-    {
-        $this->endSha = $endSha;
-
-        return $this;
-    }
-
-    /**
      * @param string $userName
      * @param string $repository
      * @param string $startSha
