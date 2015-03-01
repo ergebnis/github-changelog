d1ec54598f7bad5a91d6f76762efcbe7d410f1ff
Andreas Möller <am@localheinz.com>
2015-03-01 18:17:27 +0100

    Enhancement: Specify command arguments

--- src/Console/ChangeLogCommand.php
+++ src/Console/ChangeLogCommand.php
@@ -3,6 +3,7 @@
 namespace Localheinz\GitHub\ChangeLog\Console;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ChangeLogCommand extends Command
 {
@@ -11,6 +12,26 @@ class ChangeLogCommand extends Command
         $this
             ->setName('localheinz:changelog')
             ->setDescription('Creates a changelog based on references')
+            ->addArgument(
+                'vendor',
+                InputArgument::REQUIRED,
+                'The name of the vendor, e.g., "localheinz"'
+            )
+            ->addArgument(
+                'package',
+                InputArgument::REQUIRED,
+                'The name of the package, e.g. "github-changelog"'
+            )
+            ->addArgument(
+                'start',
+                InputArgument::REQUIRED,
+                'The start reference, e.g. "1.0.0"'
+            )
+            ->addArgument(
+                'end',
+                InputArgument::REQUIRED,
+                'The end reference, e.g. "1.1.0"'
+            )
         ;
     }
 }
--- tests/Console/ChangeLogCommandTest.php
+++ tests/Console/ChangeLogCommandTest.php
@@ -4,6 +4,7 @@ namespace Localheinz\GitHub\ChangeLog\Test\Console;
 
 use Localheinz\GitHub\ChangeLog\Console;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ChangeLogCommandTest extends PHPUnit_Framework_TestCase
 {
@@ -30,5 +31,53 @@ class ChangeLogCommandTest extends PHPUnit_Framework_TestCase
     public function testDescription()
     {
         $this->assertSame('Creates a changelog based on references', $this->command->getDescription());
+    }
+
+    /**
+     * @dataProvider providerArgument
+     *
+     * @param string $name
+     * @param bool $required
+     * @param string $description
+     */
+    public function testArgument($name, $required, $description)
+    {
+        $this->assertTrue($this->command->getDefinition()->hasArgument($name));
+
+        /* @var InputArgument $argument */
+        $argument = $this->command->getDefinition()->getArgument($name);
+
+        $this->assertSame($name, $argument->getName());
+        $this->assertSame($required, $argument->isRequired());
+        $this->assertSame($description, $argument->getDescription());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerArgument()
+    {
+        return [
+            [
+                'vendor',
+                true,
+                'The name of the vendor, e.g., "localheinz"',
+            ],
+            [
+                'package',
+                true,
+                'The name of the package, e.g. "github-changelog"',
+            ],
+            [
+                'start',
+                true,
+                'The start reference, e.g. "1.0.0"',
+            ],
+            [
+                'end',
+                true,
+                'The end reference, e.g. "1.1.0"',
+            ],
+        ];
     }
 }
